d8aa2a6c25fb124269b298917759b711bd6367f7
Eva Tesarova <evtesaro@microsoft.com>
2019-09-25 08:56:16 +0200

    Added MyFirstDesktopApp project with button changing colors

---
+++ MyFirstDesktopApp/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace MyFirstDesktopApp
+{
+    using System;
+    using System.Reflection;
+    using System.Windows;
+    using System.Windows.Media;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            colorChanger.Background = GetRandomBrush();
+        }
+
+        private Brush GetRandomBrush()
+        {
+            Brush result = Brushes.Transparent;
+
+            Random rnd = new Random();
+
+            Type brushesType = typeof(Brushes);
+
+            PropertyInfo[] properties = brushesType.GetProperties();
+
+            int random = rnd.Next(properties.Length);
+            result = (Brush)properties[random].GetValue(null, null);
+
+            return result;
+        }
+    }
+}
